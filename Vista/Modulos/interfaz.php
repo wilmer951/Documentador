e45906a75f1326f4f01a0110943ff1fc33513b23
wilmer951 <wilmer-951@hotmail.com>
2021-12-21 21:31:51 -0500

    V7

--- Vista/Modulos/interfaz.php
+++ Vista/Modulos/interfaz.php
@@ -11,6 +11,8 @@ if(!$_SESSION["validar"]){
 
 ?>
 
+<div class="divinterfaz"><!-- ************* INICIO DIV INTERFAZ ***********-->
+
 
 
      
@@ -20,25 +22,85 @@ include "menu.php";
 ?>
 
 
-<!-- ************* DIV NOTAS ***********-->
+<!-- ************* INICIO DIV NOTAS ***********-->
+	<div class="sectionnotas mb-2">
+<textarea class="form-control" name="" id="">preuba</textarea>
 
-	<div class="sectionnotas">
-
-                          <?php   
-                                  $consultanotas = new Controlador_notas();
-                                  $consultanotas -> notasControlador();
-                          ?>
-
-	</div>
-
-<!-- DIV NOTAS -->
+  </div>
+<!-- ************* FIN DIV NOTAS ***********-->
 
 
 
+<!-- ************* INICIO DIV SELECCION MODULOS ***********-->
+<div class="sectionmodulos">
+<h6>modulos</h6>
+<select class="form-select form-select-sm mb-1" name="" id="">
+<option value="">Seleccione una opción</option>
+</select>
+<select class="form-select form-select-sm mb-1" name="" id="">
+<option value="">Seleccione una opción</option>
+</select>
+<select class="form-select form-select-sm mb-1" name="" id="">
+<option value="">Seleccione una opción</option>
+
+</select>
+
+</div>
+<!-- ************* FIN DIV SELECCION MODULOS ***********-->
+
+
+
+<!-- ************* INICIO DIV INPUT ***********-->
+<div class="sectioninputs">
+  <input class="form-control form-control-sm mb-1" type="text">
+  <input class="form-control form-control-sm mb-1" type="text">
+  <input class="form-control form-control-sm mb-1" type="text">
+
+
+  <button class="btn btn-sm btnone" style="font-size:12px;">Generar</button>
+
+</div>
+<!-- ************* INICIO DIV INPUT ***********-->
 
 
 
 
+<!-- ************* INICIO DIV TIPIFICACIÓN ***********-->
+<div class="sectitipificacion text-center py-4">
+
+<button class="btn btn-sm btn-success">ver tipificacion</button>
+ 
+</div>
+<!-- ************* FIN DIV TIPIFICACIÓN ***********-->
+
+
+
+<!-- ************* INICIO BOTONES ***********-->
+    <div class="sectionbuttoncopy">
+
+    <div class="mb-1">
+        <button class="btn btn-sm btnone w-100">Descripcion</button>
+    </div>
+    <div class="mb-1">
+        <button class="btn btn-sm btnone w-100">Diagnostico</button>
+    </div>
+    <div class="mb-1">
+          <button class="btn btn-sm btnone w-100">Soluciojn</button>
+    </div>
+    <div class="mb-1">
+          <button class="btn btn-sm btnone w-100">cierre</button>
+    </div>
+
+
+</div>
+<!-- ************* FIN BOTONES ***********-->
+
+
+
+
+
+
+</div><!-- ************* FIN DIV INTERFAZ ***********-->
 
 
 
